99c83478a2d4aecb3cd43e72e164da50761d4c03
sc0Vu <alk03073135@gmail.com>
2017-10-14 10:32:54 +0800

    Add request debug and pull method Debug option(request debug) Pull method

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ var request = require('request-promise')
 module.exports = exports = function (target, options) {
   var crawler = {}
   var defaultOptions = {
+    debug: false,
     parallel: true,
     max: 10
   }
 
   crawler.options = Object.assign({}, defaultOptions, options)
   crawler.targets = []
+
+  if (crawler.options.debug === true) {
+    require('request-debug')(request)
+  }
 
   if (target) {
     crawler.targets.push(target)
@@ -23,12 +28,16 @@ module.exports = exports = function (target, options) {
     return this.targets.splice(0, 1)[0]
   }
 
+  crawler.pull = function (count) {
+    return this.targets.splice(0, count)
+  }
+
   crawler.total = function () {
     return this.targets.length
   }
 
   crawler.run = async function () {
-    var targets = this.targets.splice(0, this.options.max)
+    var targets = this.pull(this.options.max)
     var total = targets.length
 
     if (this.options.parallel) {
